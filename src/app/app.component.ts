7bfc312ee687f54d0fe1fdcc2e92840f70847d07
Smit Joshi <smitdeepjoshi@rediff.com>
2022-09-27 16:49:43 +0530

    initial stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import {BreakpointObserver} from '@angular/cdk/layout'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-news';
+export class AppComponent implements AfterViewInit {
+  @ViewChild (MatSidenav) sideNav!: MatSidenav;
+
+  constructor(
+    private observer: BreakpointObserver,
+    private cdr: ChangeDetectorRef
+    )
+    {};
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = false;
+    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+      if(res?.matches)
+      {
+        this.sideNav.mode = 'over';
+        this.sideNav.close();
+      }
+      else
+      {
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    });
+    this.cdr.detectChanges();
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {LayoutModule} from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatIconModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
